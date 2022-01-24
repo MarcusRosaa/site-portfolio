2b7f9b7f68171bdaa5e2bd41324e31747078ae9d
MarcusRosaa <marcusvrosa1@gmail.com>
2022-01-23 22:25:23 -0300

    starts portfolio section

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,7 +3,7 @@ import { Container } from './styles';
 export default function Projects() {
   return (
     <Container>
-      <h2>Projetos que fiz</h2>
+      <h2>Projetos que fiz.</h2>
     </Container>
   );
 }
